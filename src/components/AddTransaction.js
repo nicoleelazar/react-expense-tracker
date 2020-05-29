3632725935b6080930d2ecbb1179f5af89bdfba7
Nicole Elazar <nicole.elazar@gmail.com>
2020-05-29 14:45:20 +0200

    add new transaction to list

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,13 +1,32 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
+import { v4 as uuidv4 } from "uuid";
 
 const AddTransaction = () => {
     const [text, setText] = useState("");
     const [amount, setAmount] = useState(0);
 
+    const { addTransaction } = useContext(GlobalContext);
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+
+        const newTransaction = {
+            id: uuidv4(),
+            text: text,
+            amount: parseInt(amount), //we need a number not string from the value input
+        };
+
+        //add trans to list & clear value fields
+        addTransaction(newTransaction);
+        setText("");
+        setAmount(0);
+    };
+
     return (
         <div>
             <h4 className="sub-heading">Add new Transaction</h4>
-            <form className="form">
+            <form className="form" onSubmit={onSubmit}>
                 <div className="form-section">
                     <label htmlFor="description">Description</label>
                     <input
--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -1,8 +1,24 @@
-//reducer specifies apps state changes
+//reducer specifies state changes and sends them down to app
 
 export default (state, action) => {
     switch (action.type) {
+        case "DELETE_TRANSACTION":
+            //set the new state to be all transactions except the one that was deleted
+            return {
+                ...state,
+                transactions: state.transactions.filter(
+                    (item) => item.id !== action.payload
+                ),
+            };
+
+        case "ADD_TRANSACTION":
+            return {
+                ...state,
+                //adding the new trans. sent as payload + existing transactions
+                transactions: [action.payload, ...state.transactions],
+            };
+
         default:
-            return state
+            return state;
     }
-}
+};
